9cecaadad8920210eef06e1dc09863ef595e78a6
ale-rt <alessandro.pisa@gmail.com>
2023-02-09 13:01:21 +0100

    Remove not needed boilerplate

--- src/plone/classicui/tests/test_setup.py
+++ src/plone/classicui/tests/test_setup.py
@@ -1,6 +1,3 @@
-from plone import api
-from plone.app.testing import setRoles
-from plone.app.testing import TEST_USER_ID
 from plone.base.utils import get_installer
 from plone.classicui.testing import PLONE_CLASSICUI_INTEGRATION_TESTING
 
@@ -35,10 +32,7 @@ class TestUninstall(unittest.TestCase):
     def setUp(self):
         self.portal = self.layer["portal"]
         self.installer = get_installer(self.portal, self.layer["request"])
-        roles_before = api.user.get_roles(TEST_USER_ID)
-        setRoles(self.portal, TEST_USER_ID, ["Manager"])
         self.installer.uninstall_product("plone.classicui")
-        setRoles(self.portal, TEST_USER_ID, roles_before)
 
     def test_product_uninstalled(self):
         """Test if plone.classicui is cleanly uninstalled."""
