5f3ba025c39e3004b43180c6a36b3d198e6d1278
Gil Forcada Codinachs <gil.gnome@gmail.com>
2025-11-21 23:36:52 +0100

    Switch to PEP 420 native namespace.

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-from setuptools import find_packages
 from setuptools import setup
 
 
@@ -20,7 +19,6 @@ setup(
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Environment :: Web Environment",
-        "Framework :: Plone :: 6.1",
         "Framework :: Plone :: 6.2",
         "Framework :: Plone :: Distribution",
         "Framework :: Plone",
@@ -42,14 +40,10 @@ setup(
         "Tracker": "https://github.com/plone/plone.classicui/issues",
     },
     license="GPL version 2",
-    packages=find_packages("src"),
-    namespace_packages=["plone"],
-    package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
     python_requires=">=3.10",
     install_requires=[
-        "setuptools",
         "plone.distribution",
         "plone.base",
         "Zope",
